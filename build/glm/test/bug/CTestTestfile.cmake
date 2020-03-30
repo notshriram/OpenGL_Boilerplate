a0ae7494fbb5e8b2462d21a7ec413c5cc585ced0
Shriram-WinDesktop <shriram.ravi@aol.com>
2020-03-30 12:04:12 +0530

    Added all subdirectories

---
+++ build/glm/test/bug/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/Stewie/OpenGL_Boilerplate/glm/test/bug
+# Build directory: C:/Users/Stewie/OpenGL_Boilerplate/build/glm/test/bug
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.

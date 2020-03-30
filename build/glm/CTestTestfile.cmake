093165a612b2ae49c38edebd141b1da6980809ef
Shriram-WinDesktop <shriram.ravi@aol.com>
2020-03-30 18:36:42 +0530

    removed folder

--- build/glm/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/Stewie/Desktop/Opengl_boilerplate/glm
-# Build directory: C:/Users/Stewie/Desktop/Opengl_boilerplate/build/glm
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("glm")
-subdirs("test")
--- build/glm/glmConfig.cmake
+++
@@ -1,6 +0,0 @@
-set(GLM_VERSION "0.9.8")
-set(GLM_INCLUDE_DIRS "C:/Users/Stewie/Desktop/Opengl_boilerplate/glm")
-
-if (NOT CMAKE_VERSION VERSION_LESS "3.0")
-    include("${CMAKE_CURRENT_LIST_DIR}/glmTargets.cmake")
-endif()

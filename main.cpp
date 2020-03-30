e0af67932241e0a2591e7c2667e154903601ae3c
Shriram-WinDesktop <shriram.ravi@aol.com>
2020-03-30 19:04:50 +0530

    removed folder

--- main.cpp
+++ main.cpp
@@ -1,7 +1,10 @@
-#define GLEW_STATIC
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 #include <iostream>
+#include "imgui.h"
+#include "imgui_impl_glfw.h"
+#include "imgui_impl_opengl3.h"
+const char* glsl_version = "#version 130";
 int main(void)
 {
 	GLFWwindow* window;
@@ -20,27 +23,48 @@ int main(void)
 
 	/* Make the window's context current */
 	glfwMakeContextCurrent(window);
-	
-	if (glewInit() != GLEW_OK)std::cout << "error";
 
+	if (glewInit() != GLEW_OK)std::cerr << "Glew init fail";
+
+	ImGui::CreateContext();
+	ImGui::StyleColorsDark();
+	ImGui_ImplGlfw_InitForOpenGL(window, true);
+	ImGui_ImplOpenGL3_Init(glsl_version);
+
+	ImVec4 clear_color = ImColor(0x49,0xA6,0x95);
 	/* Loop until the user closes the window */
 	while (!glfwWindowShouldClose(window))
 	{
 		/* Render here */
-		glClear(GL_COLOR_BUFFER_BIT);
+		
+		ImGui_ImplOpenGL3_NewFrame();
+		ImGui_ImplGlfw_NewFrame();
+		ImGui::NewFrame();
+		
+		{
+			static float f = 0.0f;
+			ImGui::Text("Hello, world!");
+			ImGui::SliderFloat("float", &f, 0.0f, 1.0f);
+			ImGui::ColorEdit3("clear color", (float*)& clear_color);
+			ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
+		}
 
-		glBegin(GL_TRIANGLES);
-		glVertex2f( 0.5,-0.5 );
-		glVertex2f( 0,   0.5 );
-		glVertex2f(-0.5,-0.5);
-		glEnd();
+
+		ImGui::Render();
+		glClearColor(clear_color.x, clear_color.y, clear_color.z, clear_color.w);
+		glClear(GL_COLOR_BUFFER_BIT);
+		ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
 		/* Swap front and back buffers */
 		glfwSwapBuffers(window);
 
 		/* Poll for and process events */
 		glfwPollEvents();
 	}
+	ImGui_ImplOpenGL3_Shutdown();
+	ImGui_ImplGlfw_Shutdown();
+	ImGui::DestroyContext();
 
+	glfwDestroyWindow(window);
 	glfwTerminate();
 	return 0;
 }
